691a7580caa0331f6dc2ff3713be626b996dbe6d
Shadowlink <Sordstriker_@hotmail.com>
2019-10-14 13:38:18 +0200

    Healthbars added! adaptive based on health, assignable to player, enemy, future lbs

---
+++ Legend of Sephraih/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+
+    
+
+    private HealthController hc;
+    public Image fgi;
+
+    private void Start()
+    {
+        hc = GetComponentInParent<HealthController>();
+    }
+
+    private void Update()
+    {
+        fgi.fillAmount = 1.0f / ((float)hc.MaxHealth / (float)hc.health);
+    }
+
+
+}
